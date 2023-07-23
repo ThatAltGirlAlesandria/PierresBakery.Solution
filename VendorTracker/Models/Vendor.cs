1b5b9e691370f9366ebd8fe6a608854e37e02556
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-23 15:43:16 -0700

    Added variables for Vendor.

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -2,5 +2,22 @@ using System.Collections.Generic;
 
 namespace VendorTracker.Models
 {
-  
+  public class Order{
+
+    private static List<Order> _intances = new List<Order> {};
+
+    public string VendorName {get; set;}
+    public string VendorDescription {get; set;}
+    public int IdNumber {get;}
+    public List<VendorOrder> VendorOrder {get; set;}
+
+    public Order(string name, string decription)
+    {
+      VendorName = name;
+      VendorDescription = decription;
+      instances.Add(this);
+      IdNumber = _instances.Count;
+      VendorOrder = new List<VendorOrder>{};
+    }
+  }
 }
