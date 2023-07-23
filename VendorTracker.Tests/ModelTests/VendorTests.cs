b456e5fc6142da9858ca672605e269e5ef222b5e
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-23 16:36:17 -0700

    Created new test.

--- VendorTracker.Tests/ModelTests/VendorTests.cs
+++ VendorTracker.Tests/ModelTests/VendorTests.cs
@@ -3,6 +3,21 @@ using System;
 using System.Collections.Generic;
 using VendorTracker.Models;
 
-namespace VendorTracker.Tests {
-  
+namespace VendorTracker.Tests 
+{
+  [TestClass]
+  public class VendorTest : IDisposable
+  {
+    public void Clear()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void PassVendorConstructor_ToCreate_Vendor()
+    {
+      Vendor newVendor = new Vendor("Starbucks", "The Human Bean", "Dutch Brothers");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
 }
