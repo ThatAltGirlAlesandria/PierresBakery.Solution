481d8e08eaca1f6b8080673f7bb23c67b4422797
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-27 11:51:40 -0700

    All tests pass

--- VendorTracker.Tests/ModelsTests/OrderTests.cs
+++ VendorTracker.Tests/ModelsTests/OrderTests.cs
@@ -16,8 +16,95 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void PassOrderConstructor_ToCreate_Order()
     {
-      OrderTest newOrder = new OrderTest("order", "order list", "month", "day" , "year");
+      DateTime date = new DateTime(2023, 07, 27);
+      Order newOrder = new Order("Starbucks order", "Pastries", 450, date);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void ReturnsName_String()
+    {
+      string orderName = "Starbucks";
+      string orderDescription = "Pastries";
+      int orderPrice = 450;
+      DateTime date = new DateTime(2023, 07, 27);
+
+      Order newOrder = new Order(orderName, orderDescription, orderPrice, date);
+      string result = newOrder.OrderTitle;
+
+      Assert.AreEqual(orderName, result);
+    }
+
+    [TestMethod]
+    public void GetOrderDescription_ReturnsOrderDescription_String()
+    {
+      string orderName = "Starbucks";
+      string orderDescription = "Pastries";
+      int orderPrice = 450;
+      DateTime date = new DateTime(2023, 07, 27);
+
+      Order newOrder = new Order(orderName, orderDescription, orderPrice, date);
+      string result = newOrder.OrderDescription;
+
+      Assert.AreEqual(orderDescription, result);
+    }
+
+    [TestMethod]
+    public void GetsOrderPrice_ReturnsOrderPrice_Int()
+    {
+            string orderName = "Starbucks";
+      string orderDescription = "Pastries";
+      int orderPrice = 450;
+      DateTime date = new DateTime(2023, 07, 27);
+
+      Order newOrder = new Order(orderName, orderDescription, orderPrice, date);
+      int result = newOrder.OrderPrice;
+      
+      Assert.AreEqual(orderPrice, result);
+    }
+
+        [TestMethod]
+    public void GetsOrderDate_ReturnsOrderDate_DateTime()
+    {
+      string orderName = "Starbucks";
+      string orderDescription = "Pastries";
+      int orderPrice = 450;
+      DateTime date = new DateTime(2023, 07, 27);
+
+      Order newOrder = new Order(orderName, orderDescription, orderPrice, date);
+      DateTime result = newOrder.Date;
+      
+      Assert.AreEqual(date, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsList_OrderList()
+    {
+      List<Order> newOrder = new List<Order> { };
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newOrder, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllOrdersForVendor_OrderList()
+    {
+      string orderName1 = "Starbucks";
+      string orderDescription1 = "Pastries";
+      int orderPrice1 = 450;
+      DateTime date1 = new DateTime(2023, 07, 27);
+      string orderName2 = "Dutchies";
+      string orderDescription2 = "Pastries";
+      int orderPrice2 = 45;
+      DateTime date2 = new DateTime(2023, 08, 27);
+      Order newOrder1 = new Order(orderName1, orderDescription1, orderPrice1, date1);
+      Order newOrder2 = new Order(orderName2, orderDescription2, orderPrice2, date2);
+      List<Order> newOrderList = new List<Order> {newOrder1, newOrder2};
+
+      List<Order> result = Order.GetAll();
+
+      CollectionAssert.AreEqual(newOrderList, result);
     }
   }
 }
--- VendorTracker.Tests/ModelsTests/VendorTests.cs
+++ VendorTracker.Tests/ModelsTests/VendorTests.cs
@@ -16,7 +16,7 @@ namespace VendorTracker.Tests
     [TestMethod]
     public void PassVendorConstructor_ToCreateInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("Starbucks", "The Human Bean", "Dutch Brothers");
+      Vendor newVendor = new Vendor("Starbucks", "Cafe");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
@@ -25,10 +25,57 @@ namespace VendorTracker.Tests
     {
       string name = "Starbucks";
       string description = "Cafe";
-      VendorTests newVendor = new Vendor(name, description);
+      Vendor newVendor = new Vendor(name, description);
 
-      //string result = newVendor.
+      string result = newVendor.VendorName;
 
+      Assert.AreEqual(name, result);
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsTheVendorsId_Int()
+    {
+      string name = "Starbucks";
+      string description = "Cafe";
+      Vendor newVendor = new Vendor(name, description);
+
+      int result = newVendor.Id;
+
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllObjects_VendorDetails()
+    {
+      string name1 = "Starbucks";
+      string description1= "Cafe";
+      string name2 = "Dutchies";
+      string description2 = "Cafe";
+      Vendor newVendor1 = new Vendor(name1, description1);
+      Vendor newVendor2 = new Vendor(name2, description2);
+
+      Vendor result = Vendor.Find(2);
+
+      Assert.AreEqual(newVendor2, result);
+    }
+
+    [TestMethod]
+    public void AddOrder_LinksVendorWithOrders_OrderList()
+    {
+      string orderName = "Starbucks";
+      string orderDescription = "Pastries";
+      int orderPrice = 450;
+      DateTime date = new DateTime(2023, 07, 27);
+      Order newOrder = new Order(orderName, orderDescription, orderPrice, date);
+      List<Order> newList = new List<Order> {newOrder};
+      string name = "Starbucks";
+      string description = "Cafe";
+      Vendor newVendor = new Vendor(name, description);
+      newVendor.AddOrder(newOrder);
+
+      List<Order> result = newVendor.Order;
+
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
